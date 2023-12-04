c234d86c2fed5b1d73620b4a365bbee2b256c013
Munachimso Aneke <52995219+anekemuna@users.noreply.github.com>
2023-12-04 13:52:36 -0700

    DatabaseConnector class
    Updated gradle support

---
+++ BloodConnect/src/main/java/com/example/bloodconnect/DatabaseConnector.java
@@ -0,0 +1,28 @@
+package com.example.bloodconnect;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnector {
+
+    private static final String JDBC_URL = "jdbc:postgresql://localhost:5433/BloodDonationDB";
+    private static final String USER = "blooddonationdb";
+    private static final String PASSWORD = "blooddonationdb";
+
+    // Establishes a connection to the database
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(JDBC_URL, USER, PASSWORD);
+    }
+
+    // Closes the provided connection
+    public static void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace(); // Handle the exception appropriately
+            }
+        }
+    }
+}
--- BloodConnect/src/main/java/module-info.java
+++ BloodConnect/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.bloodconnect {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.bloodconnect to javafx.fxml;
